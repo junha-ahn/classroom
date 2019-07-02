2c843673fa1025fe35e10893fe209bf231448328
junha-ahn <junha.ahn.dev@gmail.com>
2019-07-02 13:30:50 +0900

    serializeQuery

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,17 +2,18 @@ function makeGlobal() {
   return {
     ajax: function (object, done, err) {
       var url = object.url;
-      var type = object.type;
+      var type = object.type || 'get';
       var data = object.data;
+      var query = object.query;
       if (data != null) {
         data = JSON.stringify(data);
       }
       $.ajax({
-        url,
-        type,
+        url: url + serializeQuery(query),
+        type: type,
         cache: false,
         contentType: "application/json; charset=UTF-8",
-        data,
+        data: data,
         success: function (rd) {
           var status;
           done(rd, status);
@@ -27,3 +28,15 @@ function makeGlobal() {
     }
   }
 }
+
+function serializeQuery(query) {
+  var data = Object.keys(query);
+  return data.length
+  ? '?' + data.map(function(key){
+    var value = query[key];
+    return (value !== '' && value !== null && value !== undefined)
+    ? key + '=' + value
+    : ''
+  }).join('&')
+  : '';
+};
