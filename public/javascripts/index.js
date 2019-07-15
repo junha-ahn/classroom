1bb1cb3192aa3f793b98038bc02bad7fc47927e5
junha-ahn <junha.ahn.dev@gmail.com>
2019-07-15 17:52:13 +0900

    ..

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,6 +5,7 @@ function makeGlobal() {
   var resetTime = function(date) {
     date.setHours(0);
     date.setMinutes(0);
+    date.setSeconds(0);
     date.setMilliseconds(0);
     return date;
   }
